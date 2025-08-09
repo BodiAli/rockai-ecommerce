d5d755097676aef04dd8a3263cce47e42ba5ad35
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-08-09 15:52:08 +0300

    Create controllers for updating and deleting products

--- src/controllers/productsController.ts
+++ src/controllers/productsController.ts
@@ -5,15 +5,9 @@ import fs from "node:fs/promises";
 import * as productsModel from "../models/productsModel.js";
 import validationErrors from "./validationErrors.js";
 import cloudinary from "../config/cloudinaryConfig.js";
+import type Product from "../types/types.ts";
 
 const upload = multer({ dest: "uploads" });
-
-interface Product {
-  title: string;
-  description: string;
-  price: number;
-  category: string;
-}
 
 const validatePageQuery = [
   query("page").customSanitizer(async (value: string) => {
@@ -51,6 +45,21 @@ export const getProducts = [
   },
 ] as RequestHandler[];
 
+export const getProduct = async (req: Request<{ id: string }>, res: Response) => {
+  const id = Number.parseInt(req.params.id);
+
+  const product = await productsModel.getProduct(id);
+
+  if (!product) {
+    res
+      .status(404)
+      .json({ error: "Product not found! it may have been moved, deleted, or it might have never existed." });
+    return;
+  }
+
+  res.status(200).json({ product });
+};
+
 const validateProduct = [
   body("title")
     .trim()
@@ -71,6 +80,11 @@ const validateProduct = [
     .withMessage(`Category ${validationErrors.emptyErr}`)
     .isLength({ max: 100 })
     .withMessage(`Category ${validationErrors.maxLengthErr100}`),
+];
+
+export const createProduct = [
+  upload.single("productImage"),
+  validateProduct,
   body("productImage").custom(async (value, { req }) => {
     const mReq = req as Request & { file: Express.Multer.File | undefined };
 
@@ -90,11 +104,6 @@ const validateProduct = [
     }
     return true;
   }),
-];
-
-export const createProduct = [
-  upload.single("productImage"),
-  validateProduct,
   async (
     req: Request<object, object, Product> & { file: Express.Multer.File },
     res: Response,
@@ -112,7 +121,10 @@ export const createProduct = [
     let imgUrl: string;
 
     try {
-      const { secure_url: url, public_id: id } = await cloudinary.uploader.upload(req.file.path);
+      const { secure_url: url, public_id: id } = await cloudinary.uploader.upload(req.file.path, {
+        resource_type: "image",
+        format: "jpg",
+      });
       imgUrl = url;
       cloudId = id;
     } catch (err) {
@@ -125,15 +137,143 @@ export const createProduct = [
 
     const { title, description, price, category } = req.body;
 
-    const createdProduct = await productsModel.createProduct(
+    const createdProduct = await productsModel.createProduct({
       title,
       description,
       price,
       category,
       imgUrl,
-      cloudId
-    );
+      cloudId,
+    });
 
     res.status(201).json({ msg: "Product created successfully!", createdProduct });
   },
 ] as RequestHandler[];
+
+export const updateProduct = [
+  upload.single("productImage"),
+  validateProduct,
+  body("productImage")
+    .optional({ values: "falsy" })
+    .custom(async (value, { req }) => {
+      const mReq = req as Request & { file: Express.Multer.File | undefined };
+
+      if (!mReq.file) {
+        return true;
+      }
+
+      if (mReq.file.size > 3145728) {
+        await fs.rm(mReq.file.path);
+        throw new Error("File cannot be larger than 3MB.");
+      } else if (!mReq.file.mimetype.startsWith("image/")) {
+        await fs.rm(mReq.file.path);
+        throw new Error("File uploaded is not of type image.");
+      } else if (mReq.file.size === 0) {
+        await fs.rm(mReq.file.path);
+        throw new Error("File cannot be empty.");
+      }
+      return true;
+    }),
+  async (
+    req: Request<{ id: string }, object, Product> & { file: Express.Multer.File | undefined },
+    res: Response,
+    next: NextFunction
+  ) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+      if (req.file) {
+        await fs.rm(req.file.path);
+      }
+      return;
+    }
+
+    const id = Number.parseInt(req.params.id);
+
+    const product = await productsModel.getProduct(id);
+
+    if (!product) {
+      res.status(404).json({
+        error: "Product not found! it may have been moved, deleted or it might have never existed.",
+      });
+
+      if (req.file) {
+        await fs.rm(req.file.path);
+      }
+      return;
+    }
+
+    let { cloudId } = product;
+    let { imgUrl } = product;
+
+    if (req.file) {
+      try {
+        const { result } = (await cloudinary.uploader.destroy(cloudId, { resource_type: "image" })) as {
+          result: string;
+        };
+
+        if (result !== "ok") {
+          throw new Error("Error updating product, please try again later.");
+        }
+
+        const { secure_url: url, public_id: id } = await cloudinary.uploader.upload(req.file.path, {
+          resource_type: "image",
+          format: "jpg",
+        });
+
+        imgUrl = url;
+        cloudId = id;
+      } catch (err) {
+        await fs.rm(req.file.path);
+        next(err);
+        return;
+      } finally {
+        await fs.rm(req.file.path);
+      }
+    }
+
+    const { title, description, price, category } = req.body;
+
+    const createdProduct = await productsModel.updateProduct({
+      id,
+      title,
+      description,
+      price,
+      category,
+      imgUrl,
+      cloudId,
+    });
+
+    res.status(200).json({ msg: "Product updated successfully!", createdProduct });
+  },
+] as RequestHandler[];
+
+export const deleteProduct = async (req: Request<{ id: string }>, res: Response, next: NextFunction) => {
+  const id = Number.parseInt(req.params.id);
+  const product = await productsModel.getProduct(id);
+
+  if (!product) {
+    res
+      .status(404)
+      .json({ error: "Product not found! it may have been moved, deleted or it might have never existed." });
+    return;
+  }
+
+  try {
+    const { result } = (await cloudinary.uploader.destroy(product.cloudId, { resource_type: "image" })) as {
+      result: string;
+    };
+
+    if (result !== "ok") {
+      throw new Error("Error deleting product, please try again later.");
+    }
+  } catch (err) {
+    next(err);
+    return;
+  }
+
+  await productsModel.deleteProduct(id);
+
+  res.sendStatus(204);
+};

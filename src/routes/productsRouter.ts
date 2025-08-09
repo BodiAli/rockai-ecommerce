cd447b1828eb5b800d9e2a658967b1d7be59207f
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-08-09 15:53:41 +0300

    Handle requests for getting a single product && put, and delete products

--- src/routes/productsRouter.ts
+++ src/routes/productsRouter.ts
@@ -4,5 +4,10 @@ import * as productsController from "../controllers/productsController.js";
 const productsRouter = Router();
 
 productsRouter.route("/products").get(productsController.getProducts).post(productsController.createProduct);
+productsRouter
+  .route("/products/:id")
+  .get(productsController.getProduct)
+  .put(productsController.updateProduct)
+  .delete(productsController.deleteProduct);
 
 export default productsRouter;

0ac00bbeaf4c5cb80f0218d4b988dd625f092d06
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-08-09 17:09:15 +0300

    Initialize react project

---
+++ frontend/src/main.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./reset.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLDivElement).render(
+  <React.StrictMode></React.StrictMode>
+);

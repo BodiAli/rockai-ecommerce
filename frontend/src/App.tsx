119650e9d706126dee8d9e1114288f1f86b945ba
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-08-10 11:05:35 +0300

    Feat: Create logic inside App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,21 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
+import { ToastContainer } from "react-toastify";
 import Header from "./components/Header/Header";
+import useGetProducts from "./utils/customHooks";
 
 export default function App() {
+  const location = useLocation();
+
+  const queryString = new URLSearchParams(location.search);
+  const currentPage = Number.parseInt(queryString.get("page") ?? "1");
+
+  const [products, dispatch, pages] = useGetProducts(currentPage);
+
   return (
     <>
       <Header />
-      <Outlet />
+      <Outlet context={{ products, pages, dispatch, currentPage }} />
+      <ToastContainer hideProgressBar />
     </>
   );
 }

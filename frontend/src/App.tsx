c4d44b81638dc0e61e13fbca1045213f9a789d38
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-08-10 06:52:52 +0300

    Feat: Render Header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,11 @@
 import { Outlet } from "react-router";
+import Header from "./components/Header/Header";
 
 export default function App() {
-  return <Outlet />;
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
 }

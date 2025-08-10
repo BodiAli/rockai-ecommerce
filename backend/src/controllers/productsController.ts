eae2088ccc1df8ead06ade8364766f4cb7b1a694
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-08-10 10:16:51 +0300

    Fix: Prevent symbols

--- backend/src/controllers/productsController.ts
+++ backend/src/controllers/productsController.ts
@@ -76,6 +76,8 @@ const validateProduct = [
     .trim()
     .notEmpty()
     .withMessage(`Price ${validationErrors.emptyErr}`)
+    .isNumeric({ no_symbols: true })
+    .withMessage(validationErrors.priceError)
     .isLength({ max: 10 })
     .withMessage(validationErrors.priceError)
     .isDecimal({ decimal_digits: "2", force_decimal: false })

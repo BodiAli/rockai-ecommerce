ce6badaf46ff1b2a0b32f8d4e4179dddb4906236
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-08-10 14:48:51 +0300

    Feat: Create 404 not found page

--- frontend/src/pages/Notfound/Notfound.tsx
+++ frontend/src/pages/Notfound/Notfound.tsx
@@ -1,3 +1,13 @@
-export default function Notfound() {
-  return <h1>Not found</h1>;
+import { Link } from "react-router";
+import styles from "./NotFound.module.css";
+
+export default function NotFound() {
+  return (
+    <main className={styles.main}>
+      <h1>404 Not found</h1>
+      <p>
+        Looks like you're lost! <Link to="/">Head back.</Link>
+      </p>
+    </main>
+  );
 }

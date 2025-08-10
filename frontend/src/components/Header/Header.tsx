b07c73fc059d58d832fb7d0da43761c8cb8df797
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-08-10 06:53:28 +0300

    Feat: Create Header component

---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,32 @@
+import { NavLink } from "react-router";
+import styles from "./Header.module.css";
+import homeIcon from "../../assets/images/home-icon.svg";
+import cartIcon from "../../assets/images/cart-icon.svg";
+import manageIcon from "../../assets/images/manage-icon.svg";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <ul>
+        <li>
+          <NavLink to="/" viewTransition>
+            <img src={homeIcon} alt="Home page" />
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/cart" viewTransition>
+            <img src={cartIcon} alt="Cart page" />
+            Cart
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/admin" viewTransition>
+            <img src={manageIcon} alt="Manage products page" />
+            Admin
+          </NavLink>
+        </li>
+      </ul>
+    </header>
+  );
+}

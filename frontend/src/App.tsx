8c8d25c0579b0b63f24c59e2bd44bdbbfabfa2b6
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-08-10 13:48:27 +0300

    Feat: Create cart state from localStorage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import { Outlet, useLocation } from "react-router";
 import { ToastContainer } from "react-toastify";
 import Header from "./components/Header/Header";
 import useGetProducts from "./utils/customHooks";
+import type { Product } from "./types/types";
 
 export default function App() {
   const location = useLocation();
+
+  const localStorageCart = localStorage.getItem("cart");
+
+  const [cart, setCart] = useState<Product[]>(
+    localStorageCart ? (JSON.parse(localStorageCart) as Product[]) : []
+  );
 
   const queryString = new URLSearchParams(location.search);
   const currentPage = Number.parseInt(queryString.get("page") ?? "1");
@@ -14,7 +22,7 @@ export default function App() {
   return (
     <>
       <Header />
-      <Outlet context={{ products, pages, dispatch, currentPage, fetchProducts }} />
+      <Outlet context={{ products, pages, dispatch, currentPage, fetchProducts, cart, setCart }} />
       <ToastContainer hideProgressBar />
     </>
   );

00c38bcb6ac75e8987fcb60d43fc3b2c4041968b
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-08-10 12:19:18 +0300

    Feat: Handle updating and deleting && return fetch products to re-fetch products on each update

--- frontend/src/utils/productsReducer.ts
+++ frontend/src/utils/productsReducer.ts
@@ -6,8 +6,16 @@ export default function productsReducer(state: Product[], action: ACTION_TYPE) {
       return action.payload;
     case "add-product":
       return [action.payload, ...state];
-
-    default:
-      throw new Error(`Unknown action ${action.type}`);
+    case "delete-product":
+      return state.filter((product) => {
+        return product.id !== action.payload.id;
+      });
+    case "update-product":
+      return state.map((product) => {
+        if (product.id === action.payload.id) {
+          return action.payload;
+        }
+        return product;
+      });
   }
 }
